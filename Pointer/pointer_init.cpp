2f0946ffdcdf18d7efb3c3ea6103eaabc7d7cd54
kushagr <kushagrsinghdec1@gmail.com>
2020-08-18 22:10:02 +0530

    modified

--- Pointer/pointer_init.cpp
+++ Pointer/pointer_init.cpp
@@ -1,2 +1,21 @@
 #include<iostream>
-using name
+using namespace std;
+int main()
+{
+    int a;
+    int *aptr;
+    a=10;
+    aptr=&a;        //stores the address of int variable
+    int x;
+    x=20;
+    cout<<&x<<endl;     //prints the address of int variable
+    char b;
+    b='a';
+    cout<<(void *)&b<<endl;     //doesn't work on char variable , thus have to explicitly typecast from char* to void*
+    char *c;
+    int d;
+    d=40;
+    // c=&d;               //results in deferencing
+    cout<<aptr<<endl;
+    return 0;
+}
