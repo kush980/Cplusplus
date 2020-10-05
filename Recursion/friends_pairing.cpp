a90e55aaf8b48193c451cebc3e6f4fa82c6593c7
kushagr <kushagrsinghdec1@gmail.com>
2020-10-05 17:21:00 +0530

    problems on recursion

---
+++ Recursion/friends_pairing.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+
+int pairing(int n)
+{
+    if(n==0 || n==1)
+    return 1;
+    return pairing(n-1)+((n-1)*pairing(n-2));
+}
+
+int main()
+{
+    int n;
+    cin>>n;
+    cout<<pairing(n)<<endl;
+}
