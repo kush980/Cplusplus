cd2bce3caa088f2de874f9099957edc5ff3f7090
kushagr <kushagrsinghdec1@gmail.com>
2020-10-15 19:10:07 +0530

    -a

---
+++ Recursion/Backtracking/tempCodeRunnerFile.cpp
@@ -0,0 +1,19 @@
+void permute(char *a,int i)
+{
+    //base case
+    if(a[i]=='\0')
+    {
+        cout<<a<<" ";
+        return;
+    }
+
+    //recursive case
+    for(int j=i;a[j]!='\0';j++)
+    {
+        swap(a[i],a[j]);
+
+        //code written above the recursive call of the function are for downward movement in the tree after that its for the upward movement
+        permute(a,i+1);
+        swap(a[i],a[j]);
+    }
+}
