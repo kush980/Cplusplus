c14aef29f5720102e51144c1978aeff59ee29a75
kushagr <kushagrsinghdec1@gmail.com>
2020-11-25 00:22:42 +0530

    modified functional objects

--- Heaps/functional_objects.cpp
+++ Heaps/functional_objects.cpp
@@ -1,5 +1,6 @@
 #include<iostream>
 #include<cstring>
+#include<queue>
 using namespace std;
 
 class fun
@@ -11,8 +12,58 @@ class fun
         }
 };
 
+class person 
+{
+    public:
+        string name;
+        int age;
+        person()
+        {
+
+        }
+        person(string n,int a)
+        {
+            name = n;
+            age = a;
+        }
+};
+
+class personCompare         //STL only accepts class not functions
+{
+    public:
+        bool operator()(person a,person b)
+        {
+            return a.age < b.age;
+        }
+
+};
+
 int main()
 {
-    fun f;
+    /*fun f;
     f("c++");   //overloaded () operator = function call in the fun class
+    */
+
+    int n;
+    cin>>n;
+    priority_queue<person , vector<person> , personCompare> pq; 
+    for(int i=0;i<n;i++)
+    {
+        string name;
+        int no;
+        cin>>name>>no;
+        person p(name,no);
+        pq.push(p);
+    }
+    int k;
+    cin>>k;
+
+    for(int i=0;i<k;i++)        //here priority queue is best option considering time complexity
+    {
+        person p = pq.top();
+        cout<<p.name<<" "<<p.age<<endl;
+        pq.pop();
+    }
+
+
 }
