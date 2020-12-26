86539de6515ad00a7bbbe0ec586b1ed9dff9f2b6
kushagr <kushagrsinghdec1@gmail.com>
2020-12-26 20:34:08 +0530

    Maximum Subarray using DP

---
+++ Dynamic Programming/Max_subarray.cpp
@@ -0,0 +1,36 @@
+#include<iostream>
+using namespace std;
+
+int maxSum(int a[],int n)
+{
+    int dp[100] = {0};
+
+    dp[0] = a[0]>0?a[0]:0;
+    int max1 = dp[0];
+
+    for(int i=1;i<n;i++)
+    {
+        dp[i] = dp[i-1]+a[i];
+        if(dp[i]<0)
+        {
+            dp[i] = 0;
+        }
+        max1 = max(max1,dp[i]);
+    }
+
+    return max1;
+}
+
+
+int main()
+{
+    int n;
+    cin>>n;
+    int a[n];
+    for(int i=0;i<n;i++)
+    {
+        cin>>a[i];
+    }
+
+    cout<<maxSum(a,n)<<endl;
+}
