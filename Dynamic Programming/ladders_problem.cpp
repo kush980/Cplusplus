6e526295ef1efc3c7b0255a3b1d7c209ed0686e6
kushagr <kushagrsinghdec1@gmail.com>
2020-12-27 17:37:49 +0530

    Ladder Problem Optimised Approach

--- Dynamic Programming/ladders_problem.cpp
+++ Dynamic Programming/ladders_problem.cpp
@@ -29,6 +29,47 @@ int laddersProblem(int n,int k,int dp[])
 
 }
 
+int LadderProblemBU(int n,int k)
+{
+    int dp[100] = {0};
+
+    dp[0] = 1;
+
+    for(int i=1;i<=n;i++)
+    {
+        dp[i] = 0;
+        for(int j=1;j<=k;j++)
+        {
+            if(i-j>=0)
+            {
+                dp[i]+=dp[i-j]; 
+            }
+        }
+    }
+    return dp[n];
+}
+
+int ladders_Optimised(int n,int k)
+{
+    int dp[100] = {0};
+
+    dp[0] = dp[1] = 1;
+    for(int i=2;i<=k;i++)
+    {
+        dp[i]=2*dp[i-1];
+    }
+    for(int i=k+1;i<=n;i++)
+    {
+        dp[i]= 2*dp[i-1] - dp[i-k-1];
+    }
+
+    for(int i=0;i<n;i++)
+    {
+        cout<<dp[i]<<" ";
+    }
+
+    return dp[n];
+}
 
 int main()
 {
@@ -36,5 +77,5 @@ int main()
     cin>>n>>k;
 
     int dp[100] = {0};
-    cout<<"No. of ways are : "<<laddersProblem(n,k,dp)<<endl;
+    cout<<"No. of ways are : "<<ladders_Optimised(n,k)<<endl;
 }
